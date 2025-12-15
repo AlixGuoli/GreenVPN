ecf17adddfb2bb60e00c2a1ff6319c3c70dd31a7
Vincent88 <641785255@qq.com>
2025-12-15 17:08:23 +0800

    Initial Commit

---
+++ GreenVPN/GreenVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenVPNApp.swift
+//  GreenVPN
+//
+//  Created by sister on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct GreenVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
