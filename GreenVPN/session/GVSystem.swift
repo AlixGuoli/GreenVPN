ee9182b08a482799cadb229f74841e3653c9b1be
Vincent88 <641785255@qq.com>
2026-03-26 10:57:58 +0800

    改名

--- GreenVPN/session/GVSystem.swift
+++ GreenVPN/session/GVSystem.swift
@@ -33,8 +33,8 @@ class GVSystem {
             if managers.count == 0 {
                 let providerManager = NETunnelProviderManager()
                 providerManager.protocolConfiguration = NETunnelProviderProtocol()
-                providerManager.localizedDescription = "Супер VPN"
-                providerManager.protocolConfiguration?.serverAddress = "Супер VPN"
+                providerManager.localizedDescription = "Speed VPN"
+                providerManager.protocolConfiguration?.serverAddress = "Speed VPN"
                 providerManager.saveToPreferences { error in
                     guard error == nil else {
                         completion(error)
